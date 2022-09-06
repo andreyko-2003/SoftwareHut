845e9f2be55b6638d6559a99f10e12453b0d4cae
Andreyko2003 <andreyko.ishchenko@gmail.com>
2022-09-06 12:24:20 +0200

    Add Loader

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Loader from './Loader';
 import {useParams} from 'react-router-dom';
 import {getPostById} from '../functions/getPosts';
 import months from '../functions/months';
@@ -8,16 +9,20 @@ function PostPage() {
   const params = useParams();
   const [post, setPost] = React.useState([]);
   const [images, setImages] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
   const date = new Date(post.date);
 
   React.useEffect(() => {
     async function fetchPost() {
+      setLoading(true);
       try {
         const res = await getPostById(params.id);
         setPost(res[0]);
         setImages(res[0].image);
       } catch (err) {
         console.log(err);
+      } finally {
+        setLoading(false);
       }
     }
     fetchPost();
@@ -25,15 +30,22 @@ function PostPage() {
 
   return (
     <div className='post'>
-      <div className='postHead'><h1>{post.title}</h1><span><i>{date.getFullYear()} {months[date.getMonth()]} {date.getDate()}</i></span></div>
-      <div className='article'><pre>{post.description}</pre></div>
-      <div className='postImg'>
       {
-        images.map(image => (
-          <img key={image.url} src={image.url} alt={post.imageLabel} width='200px'/>
-        ))
+        loading
+          ? <Loader />
+          :
+          <main>
+            <div className='postHead'><h1>{post.title}</h1><span><i>{date.getFullYear()} {months[date.getMonth()]} {date.getDate()}</i></span></div>
+            <div className='article'><pre>{post.description}</pre></div>
+            <div className='postImg'>
+            {
+              images.map(image => (
+                <img key={image.url} src={image.url} alt={post.imageLabel} width='200px'/>
+              ))
+            }
+            </div>
+          </main>
       }
-      </div>
     </div>
   );
 }
--- src/components/PostsPage.js
+++ src/components/PostsPage.js
@@ -3,37 +3,50 @@ import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import FeaturedPost from './FeaturedPost';
 import MainFeaturedPost from './MainFeaturedPost';
+import Loader from './Loader'
 // Posts
 import {getPosts} from '../functions/getPosts';
 
 function PostsPage() {
   const [posts, setPosts] = React.useState([]);
   const [firstPost, setFirstPost] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
     async function fetchPosts() {
+      setLoading(true);
       try {
         const res = await getPosts();
         setFirstPost(res[0])
         setPosts(res.slice(1));
       } catch (err) {
         console.log(err);
+      } finally {
+        setLoading(false);
       }
     }
     fetchPosts();
   }, []);
 
   return (
-    <main>
-      <MainFeaturedPost post={firstPost} />
-      <Grid container spacing={5} sx={{ mt: 3 }}>
-        {
-          posts.map(post => (
-            <FeaturedPost key={post.title} post={post} />
-          ))
-        }
-      </Grid>
-    </main>
+    <div>
+    {
+      loading
+        ?
+          <Loader />
+        :
+          <main>
+            <MainFeaturedPost post={firstPost} />
+            <Grid container spacing={5} sx={{ mt: 3 }}>
+            {
+              posts.map(post => (
+                <FeaturedPost key={post.title} post={post} />
+              ))
+            }
+            </Grid>
+          </main>
+    }
+    </div>
   );
 }
 
